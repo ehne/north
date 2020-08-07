a84775efb4e97bc324015a039d5c60292377849a
Darcy LF <ehnevault@gmail.com>
2020-08-07 23:25:07 +1000

    start work on getting the users webcal

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import { TopNav } from 'bumbag'
+import { TopNav, Modal, Dialog, Button,Paragraph,Text } from 'bumbag'
 import DarkModeToggle from "./darkModeToggle"
 
 export default function Header() {
@@ -7,11 +7,26 @@ export default function Header() {
            <TopNav.Section>
             
         </TopNav.Section>
-           <TopNav.Section padding="major-2">
+           <TopNav.Section paddingY="major-2" paddingX="major-1">
+               <TopNav.Item paddingX="major-1">
+               <Modal.State>
+                <Dialog.Modal title="North Settings" type="warning" showActionButtons>
+                    <Paragraph>
+                        <Text use="strong">North</Text> requires the use of your compass calendar.
+                    </Paragraph>
+                    <Paragraph>
+                        To 
+                    </Paragraph>
+                </Dialog.Modal>
+                <Modal.Disclosure use={Button}>
+                    Setup
+                </Modal.Disclosure>
+                </Modal.State>
+               </TopNav.Item>
                 <TopNav.Item>
-                <DarkModeToggle></DarkModeToggle>
+                {/* <DarkModeToggle></DarkModeToggle> */}
                 </TopNav.Item>
-
+                
             </TopNav.Section>
        </TopNav>  
     )
